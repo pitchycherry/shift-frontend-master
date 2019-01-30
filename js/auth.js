e4e343dc35a5070f876438fed64d811edbe99761
pitchycherry <nik_kirienko@mail.ru>
2019-01-30 23:32:32 +0700

    Special commit for change lk.html to lk.js and return data of lk in index.html

--- js/auth.js
+++ js/auth.js
@@ -5,20 +5,34 @@ let token;
 function auth() {
     const addUserForm = document.querySelector("#addUser");
     addUserForm.addEventListener("submit", event => {
-        event.preventDefault();
+            event.preventDefault();
 
-        const data = getFieldData(event.target);
-        console.log("main", "data", data);
+            const data = getFieldData(event.target);
+            console.log("main", "data", data);
 
-        createRequest({ path: `http://demo6634020.mockable.io/login`, method: "POST" }, {}, data)
-            .then(response => {
-                localStorage.setItem('token', response.token);
-                location.href= "http://localhost:63342/shift-frontend-master/lk.html";
-                console.log("Пользователь аутентифицирован", response);
+            createRequest({path: `http://demo6634020.mockable.io/login`, method: "POST"}, {}, data)
+                .then(response => {
+                    localStorage.setItem('token', response.token);
+                    console.log("Токен отправлен")
+                    createRequest({path: `http://demo6634020.mockable.io/users`, method: "GET"}, {})
+                        .then(response => {
 
-            })
-            .catch(() => {
-                console.log("Пользователь не аутентифицирован");
-            });
-    });
+                            //document.querySelector(".auth-form").textContent = "jfjfjfjfj:";
+                            //location.href = "http://localhost:63342/shift-frontend-master/lk.html";
+                            document.querySelector(".profile.col-md-3").innerHTML = `<p>emvekrmvkemrvk</p>`;
+                                console.log("Данные пользователя получены", response);
+                        })
+                        .catch(() => {
+                            console.log("Данные пользователя не получены");
+                        });
+
+                    console.log("Пользователь аутентифицирован", response);
+
+                })
+                .catch(() => {
+                    console.log("Пользователь не аутентифицирован");
+                });
+        }
+    )
+
 }
